0945d07ca5f72f352060eb806f14cbc273cb3f2c
jgsheppa <jgsheppa@protonmail.com>
2025-02-10 22:55:54 +0000

    feat(repo): structure repo

---
+++ course/part1/push/push.js
@@ -0,0 +1,17 @@
+const users = [
+  { id: 1, roles: ['customer'], hasKeys: false },
+  { id: 2, roles: ['admin', 'developer'], hasKeys: true },
+  { id: 3, roles: ['customer'], hasKeys: false },
+  { id: 4, roles: ['customer'], hasKeys: false },
+  { id: 5, roles: ['admin', 'manager'], hasKeys: true },
+  { id: 6, roles: ['admin', 'manager'], hasKeys: true },
+  { id: 7, roles: ['admin', 'manager'], hasKeys: true },
+  { id: 8, roles: ['staff'], hasKeys: true },
+  { id: 9, roles: ['staff'], hasKeys: true },
+  { id: 10, roles: ['staff'], hasKeys: true },
+  { id: 11, roles: ['supplier', 'producer'], hasKeys: false },
+  { id: 12, roles: ['supplier'], hasKeys: false },
+  { id: 13, roles: ['admin', 'owner'], hasKeys: true },
+];
+
+users.push({ id: 14, roles: ['customer'], hasKeys: false });
