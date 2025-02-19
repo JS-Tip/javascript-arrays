542982c403d8189a77862fe63d14ec7d97b0b391
James Sheppard <jgsheppa@protonmail.com>
2025-02-19 10:24:37 +0000

    chore(course): add documentation for capitalize example

--- course/part8/capitalize.js
+++ course/part8/capitalize.js
@@ -1,5 +1,13 @@
+// Objective: Capitalize the first and last name
+// of Keanu Reaves using the Array methods split, map, 
+// and join.
+
 const fullName = "keanu reaves"
+
 const fullNameCapitalizedWithSlice = fullName.split(" ").map(name => {
+    // Note: the String class also has methods called at and slice,
+    // which are not to be confused with the Array methods of the same
+    // names.
     return `${name.at(0).toUpperCase()}${name.slice(1)}`
 }).join(" ")
 
@@ -10,8 +18,7 @@ console.log("full name capitalized with slice:", fullNameCapitalizedWithSlice)
 // is a callback function which will be called for on each match.
 const fullNameCapitalizedWithReplace = fullName.replace(/\w\S*/g, 
     function(name) { 
-        return name.at(0).toUpperCase() + 
-        name.slice(1);
+        return `${name.at(0).toUpperCase()}${name.slice(1)}`
     }
 );
 
